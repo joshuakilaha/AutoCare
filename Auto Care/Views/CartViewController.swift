09ecebed0ba8e4ac89bee0373cca4019961d3378
joshua kilaha <jokilaha191@gmail.com>
2020-02-16 22:13:53 +0300

    Cart -> (diable button, delete and View Item)

--- Auto Care/Views/CartViewController.swift
+++ Auto Care/Views/CartViewController.swift
@@ -29,7 +29,7 @@ class CartViewController: UIViewController {
       
       let hud = JGProgressHUD(style: .light)
       
-    
+   // var automaticallyAdjustsScrollViewInsets: Bool { get set }
     
     
     //MARK: LIFE CYCLE
@@ -44,6 +44,9 @@ class CartViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+       
+        
+        
         //TODO: check is user is logged in
         downloadCart()
         
@@ -52,10 +55,11 @@ class CartViewController: UIViewController {
     
     @IBAction func checkoutButton(_ sender: Any) {
         
+        print("Checkout button Pressed")
     }
     
     
-    //MARK: Functions
+                                        //MARK: Functions
    
     private func downloadCart()  {
     
@@ -88,6 +92,8 @@ class CartViewController: UIViewController {
             totalItems.text = "\(allItems.count)"
             priceLable.text = showprice()
         }
+        
+        CheckoutButtonStatus()
     }
     
     private func showprice() -> String {
@@ -99,8 +105,62 @@ class CartViewController: UIViewController {
         }
         return "Total: " + convertCurrency(totalprice)
     }
+    
+    
+    
+    //MARK: Button Status
+    
+    private func CheckoutButtonStatus() {
+        
+        CheckoutButtonPressed.isEnabled = allItems.count > 0
+        
+        if CheckoutButtonPressed.isEnabled {
+            CheckoutButtonPressed.backgroundColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        }
+        else {
+                disableCheckoutButton()
+        }
+        
+    }
+    
+    private func disableCheckoutButton () {
+        CheckoutButtonPressed.isEnabled = false
+        CheckoutButtonPressed.backgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+    }
+    
+    private func removeItemIdsFromCart(itemId: String) {
+        
+        for i in 0..<cart!.itemIds.count {
+            
+            if itemId == cart!.itemIds[i] {
+                
+                cart!.itemIds.remove(at: i)
+
+            return
+            }
+        }
+    }
+    
+    
+    
+       //MARK: Navigation
+       
+       private func showItemView(withItem: Item) {
+           let itemVc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "itemView") as! ItemViewController
+                  
+                  itemVc.item = withItem
+                  self.navigationController?.pushViewController(itemVc, animated: true)
+       }
+       
 
 }
+
+
+
+
+
+
+
 
 
 
@@ -118,6 +178,45 @@ extension CartViewController: UITableViewDataSource, UITableViewDelegate {
         cell.generateItemCell(allItems[indexPath.row])
         
         return cell
+    }
+    
+    
+    //showing ItemView
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        showItemView(withItem: allItems[indexPath.row])
+        
+        
+    }
+    
+    
+    
+    //MARK: UITableView Delegate
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            let itemToDelete = allItems[indexPath.row]
+            allItems.remove(at: indexPath.row)
+            tableView.reloadData()
+            
+            removeItemIdsFromCart(itemId: itemToDelete.id)
+            
+            updateCartInDatabase(cart!, withValues: [cItemIds: cart!.itemIds]) { (error) in
+                if error != nil {
+                    print("error updating cart", error!.localizedDescription)
+                }
+                self.downloadItemsFormCart()
+            }
+            
+        }
     }
     
     
